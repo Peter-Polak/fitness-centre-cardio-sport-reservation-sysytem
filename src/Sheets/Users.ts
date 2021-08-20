fc4278eeb8b03dddc15a8a9121d18930fd89bf63
Peter Polák <peter.polak.mail@gmail.com>
2021-08-20 04:59:04 +0200

    Clean up code and add comments

--- src/Sheets/Users.ts
+++ src/Sheets/Users.ts
@@ -1,49 +1,8 @@
-function getAllUsers() : Array<User>
-{
-    const usersSheet = getUsersSheet();
-    if(usersSheet == null) return [];
-    const cells = usersSheet.getDataRange().getValues();
-    
-    let users : Array<User> = [];
-    for(const row of cells)
-    {
-        const user : User =
-        {
-            emailAddress : row[0],
-            token : row[1]
-        }
-        
-        users.push(user);
-    }
-    
-    return users;
-}
-
-function getUserByEmail(emailAddress : string): User | null
-{
-    const users = getAllUsers();
-    
-    for(const user of users)
-    {
-        if(user.emailAddress == emailAddress) return user;
-    }
-    
-    return null;
-}
-
-function getUserByToken(token : string): User | null
-{
-    const users = getAllUsers();
-    
-    for(const user of users)
-    {
-        if(user.token == token.toUpperCase()) return user;
-    }
-    
-    return null;
-}
-
-function addUser(emailAddress : string = "peter.polak.mail@gmail.com") : User
+/**
+ * Generate a new random verification token for the new user and add the new user to `Users` sheet.
+ * @returns {User} Newly added user object.
+ */
+function addUser(emailAddress : string) : User
 {
     const usersSheet = getUsersSheet();
     if(usersSheet == null) return { emailAddress : "", token : "" };
@@ -83,4 +42,62 @@ function addUser(emailAddress : string = "peter.polak.mail@gmail.com") : User
     usersSheet.appendRow([user.emailAddress, user.token]);
     
     return user;
+}
+
+/**
+ * Get all users from `Users` sheet.
+ * @returns {Array<User>} Array of all users.
+ */
+function getAllUsers() : Array<User>
+{
+    const usersSheet = getUsersSheet();
+    if(usersSheet == null) return [];
+
+    const cells = usersSheet.getDataRange().getValues();
+    
+    let users : Array<User> = [];
+    for(const row of cells)
+    {
+        const user : User =
+        {
+            emailAddress : row[UsersSheet.columns.EmailAddress],
+            token : row[UsersSheet.columns.Token]
+        }
+        
+        users.push(user);
+    }
+    
+    return users;
+}
+
+/**
+ * Get user from `Users` sheet by e-mail adress.
+ * @returns {User | null} User object or null if not found.
+ */
+function getUserByEmail(emailAddress : string): User | null
+{
+    const users = getAllUsers();
+    
+    for(const user of users)
+    {
+        if(user.emailAddress == emailAddress) return user;
+    }
+    
+    return null;
+}
+
+/**
+ * Get user from `Users` sheet by token.
+ * @returns {User | null} User object or null if not found.
+ */
+function getUserByToken(token : string): User | null
+{
+    const users = getAllUsers();
+    
+    for(const user of users)
+    {
+        if(user.token == token.toUpperCase()) return user;
+    }
+    
+    return null;
 }
